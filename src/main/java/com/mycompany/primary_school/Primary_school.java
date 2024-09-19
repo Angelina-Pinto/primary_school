7894afbca1fdd5d231b2d145cc98aac65b37aa21
angelina.pg <angelinapinto1278@gmail.com>
2024-09-19 06:39:33 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/primary_school/Primary_school.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primary_school;
+
+/**
+ *
+ * @author Dell
+ */
+public class Primary_school {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
